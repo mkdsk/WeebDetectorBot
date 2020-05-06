0c579d6acca1c7bd8fd1de7add231d0e7f46d437
mkdsk <shredda763@gmail.com>
2020-05-05 20:53:02 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const client = new tmi.Client({
   },
   identity: {
     username: 'WeebDetectorBot',
-    password: 'x'
+    password: [removed]
   },
   channels: [ 'masondota2' ]
 });
@@ -18,13 +18,17 @@ const delay = 8;
 var thisMsgTime;
 var lastMsgTime = 0;
 
+const pedoEmotes = ['AYAYA', 'momoLewd', 'admiralSmug', 'aniBlush', 
+      'YuriCamp', 'ayayaJAM', 'AYAYACOG', 'admiralBaka', 'ABABABABA', 
+      'TeeHee', 'AYAYAWeird', 'AYAYAYA']
+
 client.on('message', (channel, tags, message, self) => {
   // Ignore echoed messages.
   if(self) return;
 
-  if(message.trim().includes('AYAYA') || message.trim().includes('momoLewd') || message.trim().includes('admiralSmug') || message.trim().includes('aniBlush') || message.trim().includes('YuriCamp') || message.trim().includes('ayayaJAM') || message.trim().includes('AYAYACOG') || message.trim().includes('admiralBaka') || message.trim().includes('ABABABABA')) {
+  if(pedoEmotes.some(v => message.trim().includes(v))) {
     thisMsgTime = Math.round(Date.now() / 1000);
-    if((thisMsgTime - lastMsgTime) > delay) { // cuz we havent sent a msg in (delay) seconds, so we gud now
+    if((thisMsgTime - lastMsgTime) > delay) { // we haven't sent a message in over delay seconds
         client.say(channel, `@${tags.username} PedoBear Clap`);
         lastMsgTime = Math.round(Date.now() / 1000);
     }
